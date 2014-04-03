4b40671cf4c84fbb68949f71cca0ada7f1a71a0c
chidmdxx <rachid@outlook.com>
2014-04-03 12:27:33 -0600

    RC4

---
+++ VRC4/VRC4/Model/RC4.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRC4.Model
+{
+    class RC4
+    {
+        private string key;
+        private string plaintext;
+        private string ciphertext;
+        private string work;
+
+        public string Key
+        {
+            get { return key; }
+            set { key = value; }
+        }
+        public string Plaintext
+        {
+            get { return plaintext; }
+            set { plaintext = value; }
+        }
+        public string Ciphertext
+        {
+            get { return ciphertext; }
+            set { ciphertext = value; }
+        }
+        public string Work
+        {
+            get { return work; }
+            set { work = value; }
+        }
+        public RC4()
+        {
+
+        }
+
+        public void Cipher(string plaintext)
+        {
+            Plaintext = plaintext;
+            Ciphertext = string.Empty;
+            Work = string.Empty;
+            byte[] plainBytes = Encoding.ASCII.GetBytes(Plaintext);
+            byte[] K = Encoding.ASCII.GetBytes(Key);
+            int keyLenght = K.Length;
+            byte[] S = new byte[256];
+            byte[] T = new byte[256];
+            byte[] cipherBytes = new byte[plainBytes.Length];
+            int j, i;
+            /*Initialization*/
+            for (var c = 0; c < 256; c++)
+            {
+                S[c] = (byte)(c);
+                T[c] = K[c % keyLenght];
+            }
+            /*Initial permutation of S*/
+            j = 0;
+            for (var c = 0; c < 256; c++)
+            {
+                j = (j + S[c] + T[c]) % 256;
+                byte temp = S[c];//swap (S[c], S[j]);
+                S[c] = S[j];
+                S[j] = temp;
+
+            }
+            /*Stream Generation*/
+            i = 0;
+            j = 0;
+            int count = 0;
+            foreach (byte Mi in plainBytes)
+            {
+                i = (i + 1) % 256;
+                j = (j + S[i]) % 256;
+                byte temp = S[i];//swap (S[i], S[j]);
+                S[i] = S[j];
+                S[j] = temp;
+                int t = (S[i] + S[j]) % 256;
+                byte k = S[t];
+                cipherBytes[count++] = (byte)(Mi ^ k);
+            }
+        }
+    }
+}
---
+++ VRC4/VRC4/Model/Vigenere.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+
+namespace VRC4.Model
+{
+    class Vigenere
+    {
+        private string key;
+
+        public string Key
+        {
+            get { return key; }
+            set { key = value; }
+        }
+        private string plaintext;
+
+        public string Plaintext
+        {
+            get { return plaintext; }
+            set { plaintext = value; }
+        }
+        private string ciphertext;
+
+        public string Ciphertext
+        {
+            get { return ciphertext; }
+            set { ciphertext = value; }
+        }
+
+        private string work;
+
+        public string Work
+        {
+            get { return work; }
+            set { work = value; }
+        }
+
+
+        public Vigenere()
+        {
+        }
+
+        public string Cipher(string plaintext)
+        {
+            Plaintext = plaintext;
+            Ciphertext = string.Empty;
+            Work = string.Empty;
+            byte[] plainBytes = Encoding.ASCII.GetBytes(Plaintext);
+            byte[] keyBytes = Encoding.ASCII.GetBytes(Key);
+            int keyLenght = keyBytes.Length;
+            int count = 0;
+            foreach (var letter in plainBytes)
+            {
+                int ascii = letter;
+                int k;
+                if (ascii >= 97 && ascii <= 122)
+                {
+                    k = keyBytes[count++ % keyLenght];
+                    k -= 97;
+                    ascii -= 97;
+                    ascii += k;
+                    ascii %= 26;
+                    ascii += 97;
+                    Ciphertext = Ciphertext + (char)ascii;
+                    Work += string.Format("{0}. Replaced {1} for {2} with key letter {3} \n", count, (char)letter, (char)ascii, (char)(k + 97));
+                }
+            }
+            return Ciphertext;
+        }
+
+        public string Decipher(string ciphertext)
+        {
+            Ciphertext = ciphertext;
+            Plaintext = string.Empty;
+            Work = string.Empty;
+            byte[] cipherBytes = Encoding.ASCII.GetBytes(Ciphertext);
+            byte[] keyBytes = Encoding.ASCII.GetBytes(Key);
+            int keyLenght = keyBytes.Length;
+            int count = 0;
+            foreach (var letter in cipherBytes)
+            {
+                int ascii = letter;
+                int k;
+                if (ascii >= 97 && ascii <= 122)
+                {
+                    k = keyBytes[count++ % keyLenght];
+                    k -= 97;
+                    ascii -= 97;
+                    ascii += 26;
+                    ascii -= k;
+                    ascii %= 26;
+                    ascii += 97;
+                    Plaintext = Plaintext + (char)ascii;
+                    Work += string.Format("{0}. Replaced {1} for {2} with key letter {3} \n", count, (char)letter, (char)ascii, (char)(k + 97));
+                }
+            }
+            return Plaintext;
+        }
+    }
+}
